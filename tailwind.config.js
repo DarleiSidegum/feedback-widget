20f94af27af7eec22c1d1afd65c4fcee4f6aac85
Darlei Sidegum <darleidasilvasidegum@hotmail.com>
2022-05-16 20:31:23 -0300

    ajustes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
             colors: {
                 brand: {
                     300: '#996dff',
-                    500: '#8257e6',
+                    500: '#8257e5',
                 }
             },
             borderRadius: {
