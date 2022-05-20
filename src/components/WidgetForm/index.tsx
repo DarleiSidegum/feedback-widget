f902ab243e64c15c05fa19f287251f8608790541
Darlei Sidegum <darleidasilvasidegum@hotmail.com>
2022-05-20 14:05:43 -0300

    copyright

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -1,63 +1,71 @@
-import { CloseButton } from '../CloseButton';
+import { CloseButton } from "../CloseButton";
 
-import bugImageUrl from '../../assets/bug.svg'
-import ideaImageUrl from '../../assets/idea.svg'
-import thougthImageUrl from '../../assets/thought.svg'
-import { useState } from 'react';
-import { FeedbackTypeStep } from './Steps/FeedbackTypeStep';
-import { FeedbackContentStep } from './Steps/FeedbackContentStep';
-import { FeedbackSuccesStep } from './Steps/FeedbackSuccessStep';
+import bugImageUrl from "../../assets/bug.svg";
+import ideaImageUrl from "../../assets/idea.svg";
+import thougthImageUrl from "../../assets/thought.svg";
+import { useState } from "react";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSuccesStep } from "./Steps/FeedbackSuccessStep";
 
 export const feedBackTypes = {
-    BUG:{
-        title: 'Problema',
-        source: bugImageUrl,
-        alt: 'Imagem de um inseto'
-    },
-    IDEA: {
-        title: 'Ideia',
-        source: ideaImageUrl,
-        alt: 'Imagem de uma lâmpada'
-    },
-    OTHER: {
-        title: 'Outro',
-        source: thougthImageUrl,
-        alt: 'Imagem de um balão de pensamento'
-    }
-}
+  BUG: {
+    title: "Problema",
+    source: bugImageUrl,
+    alt: "Imagem de um inseto",
+  },
+  IDEA: {
+    title: "Ideia",
+    source: ideaImageUrl,
+    alt: "Imagem de uma lâmpada",
+  },
+  OTHER: {
+    title: "Outro",
+    source: thougthImageUrl,
+    alt: "Imagem de um balão de pensamento",
+  },
+};
 
 export type FeedBackType = keyof typeof feedBackTypes;
 
-export function WidgetForm(){
+export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedBackType | null>(null);
+  const [feedbackSent, setFeedbackSent] = useState(false);
 
-    const [feedbackType, setFeedbackType] = useState<FeedBackType | null>(null);
-    const [feedbackSent, setFeedbackSent] = useState(false);
+  function handleRestartFeedback() {
+    setFeedbackSent(false);
+    setFeedbackType(null);
+  }
 
-    function handleRestartFeedback(){
-        setFeedbackSent(false);
-        setFeedbackType(null);
-    }
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      {feedbackSent ? (
+        <FeedbackSuccesStep
+          onFeedbackRestartRequested={handleRestartFeedback}
+        />
+      ) : (
+        <>
+          {!feedbackType ? (
+            <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+          ) : (
+            <FeedbackContentStep
+              feedbackType={feedbackType}
+              onFeedbackRestartRequested={handleRestartFeedback}
+              onFeedbackSent={() => setFeedbackSent(true)}
+            />
+          )}
+        </>
+      )}
 
-    return (
-        <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-            
-            {feedbackSent ? <FeedbackSuccesStep onFeedbackRestartRequested={handleRestartFeedback}/> :
-                <>
-                {!feedbackType ? (
-                    <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
-                    ) : (
-                    <FeedbackContentStep 
-                    feedbackType={feedbackType} 
-                    onFeedbackRestartRequested={handleRestartFeedback}
-                    onFeedbackSent={()=>setFeedbackSent(true)}/>
-                )}
-                </>
-            }
-
-            <footer className="text-xs text-neutral-400">
-                Feito com ♥ pela <a className="underline underline-offset-2" href="https://rocketseat.com.br">Rocketseat</a>
-            </footer>
-
-        </div>
-    )
+      <footer className="text-xs text-neutral-400">
+        Feito com ♥ por{" "}
+        <a
+          className="underline underline-offset-2"
+          href="https://github.com/DarleiSidegum"
+        >
+          Darlei Sidegum
+        </a>
+      </footer>
+    </div>
+  );
 }
